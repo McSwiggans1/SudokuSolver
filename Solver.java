66b90dcce420308f149169fd56ad88811035d9bd
YM <ymironchyk@gmail.com>
2018-02-14 17:24:28 -0800

    Updated Solver.java
    This is the final update that I will make to the sudoku solver, unless I implement another version

--- Solver.java
+++ Solver.java
@@ -2,16 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] answer = new int[][] {{5,3,4,6,7,8,9,1,2},
-                                               {6,7,2,1,9,5,3,4,8},
-                                               {1,9,8,3,4,2,5,6,7},
-                                               {8,5,9,7,6,1,4,2,3},
-                                               {4,2,6,8,5,3,7,9,1},
-                                               {7,1,3,9,2,4,8,5,6},
-                                               {9,6,1,5,3,7,2,8,4},
-                                               {2,8,7,4,1,9,6,3,5},
-                                               {3,4,5,2,8,6,1,8,9}};
-    
         int[][] puzzle1 = new int[][] {{0,0,3,0,1,0,5,0,0},
                                        {8,0,0,3,9,5,0,0,1},
                                        {1,5,0,0,0,0,0,2,7},
@@ -23,7 +13,7 @@ public class Main {
                                        {0,0,6,0,3,0,2,0,0}};
         ;
                                       
-        System.out.println("Puzzle Before: ");
+        System.out.println("Before: ");
         System.out.println();
         for (int i = 0; i < puzzle1.length; i++) {
             for(int j = 0; j < puzzle1.length; j++){
@@ -44,39 +34,29 @@ public class Main {
         
         System.out.println("Puzzle After: ");
         System.out.println();
-        for (int i = 0; i < puzzle1.length; i++) {
-            for(int j = 0; j < puzzle1.length; j++){
-              System.out.print(puzzle1[i][j] + " "); 
-              if(j % 3 == 2) System.out.print("| ");
+        if(solve(solution)){
+            for (int i = 0; i < puzzle1.length; i++) {
+                for(int j = 0; j < puzzle1.length; j++){
+                  System.out.print(puzzle1[i][j] + " "); 
+                  if(j % 3 == 2) System.out.print("| ");
+                }
+                System.out.println();
+                if(i % 3 == 2){
+                  System.out.println("----------------------");
+                }
+
             }
-            System.out.println();
-            if(i % 3 == 2){
-              System.out.println("----------------------");
-            }
-             
+        } else {
+            System.out.println("No solution could be found");
         }
-        System.out.println("Puzzle Solved, here is the solution: ");
-        System.out.println();
-        for (int i = 0; i < puzzle1.length; i++) {
-            for(int j = 0; j < puzzle1.length; j++){
-              System.out.print(solution[i][j] + " "); 
-              if(j % 3 == 2) System.out.print("| ");
-            }
-            System.out.println();
-            if(i % 3 == 2){
-              System.out.println("----------------------");
-            }
-             
-        }
+         
+        
         
     }
 
     /**
      * Simple recursive, deterministic, depth first search, backtracking algorithm for Sudoku
      * Row major format puzzle as input
-     *
-     * @param puzzle Row major format puzzle as input
-     * @return
      */
     public static boolean solve(int[][] puzzle) {
         int N = 3; // length ^ 0.25
